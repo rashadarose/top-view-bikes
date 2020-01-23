d303bcfe3b06ef25ea86ff63b2452b2862dcbcc4
rashadarose <ralfonzoro@gmail.com>
2020-01-22 23:50:27 -0500

    added methods to cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
 
-  constructor() { }
+  //constructor() { }
+    items = [];
 
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
